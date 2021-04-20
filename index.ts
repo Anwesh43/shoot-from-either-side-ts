c5c3148d91b80eea704c9ef24a4b11d61dfedbce
Anwesh43 <anweshthecool0@gmail.com>
2021-04-21 04:59:40 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -28,3 +28,40 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 }
+
+class DrawingUtil {
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawShootFromEitherSide(context : CanvasRenderingContext2D, scale : number) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const sf4 : number = ScaleUtil.divideScale(sf, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor
+        const r : number = Math.min(w, h) / ballRFactor  
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.save()
+        context.rotate(sf2 * Math.PI / 2)
+        context.fillRect(-r, (-size / 2) * sf1, 2 * r, size * sf1)
+        context.restore()
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawCircle(context, -size / 2 - r - (w / 2) * sf4, 0, r * sf3)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawSFESNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawShootFromEitherSide(context, scale)
+    }
+}
